188b7815a7495ea1331b9d65064c2f4edb9fc483
Telman <Telman@DESKTOP-NG66VHB>
2024-03-03 20:07:05 +0400

    Creating connection then database seed controller view and list show

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db; // here we create an object that retrieves all methods
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db; // and pass it to this variable in constructor to then use it in all places of class
+            
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList= _db.Categories.ToList(); // here we call it and get list of categories   
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models {
+
+    public class Category
+    {
+        [Key] // it means that Id is primary key
+        public int Id { get; set; } // but you also can evade that [Key] because by default it gives primary key for Id or TableNameId
+        [Required] // it gives required property for Name column(this column have to be not null or empty)
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+
+}
+
+
